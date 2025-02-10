2b03c51bdc0dd4627fc0162778434c9e17f6a016
Carlos <carlos.soares@shcompanytechnology.com>
2025-02-09 23:23:15 -0300

    adicionado pdf para download do composit

---
+++ src/PdfComposit/PdfComposit.jsx
@@ -0,0 +1,44 @@
+import { PDFDocument } from 'pdf-lib';
+import download from "downloadjs";
+
+import foto1 from "../imagensDuda/composit/fotocapaPDF.png";
+import foto2 from "../imagensDuda/composit/compositeverso.png";
+
+async function carregarArquivo(url) {
+    const response = await fetch(url);
+    return response.arrayBuffer();
+}
+
+export async function GerarPDF() {
+    const pdfDoc = await PDFDocument.create();
+
+    // Criar página A4 (595 x 842 pontos)
+    let page = pdfDoc.addPage([595, 842]);
+    let page2 = pdfDoc.addPage([595, 842]);
+
+    // Carregar e embutir a imagem
+    const imagemBytes = await carregarArquivo(foto1);
+    const imagemEmb = await pdfDoc.embedPng(imagemBytes);
+
+    const imagemBytes2 = await carregarArquivo(foto2);
+    const imagemEmb2 = await pdfDoc.embedPng(imagemBytes2);
+
+    // **Desenhar a imagem preenchendo toda a página**
+    page.drawImage(imagemEmb, {
+        x: 0,
+        y: 0,
+        width: 595,
+        height: 842,
+    });
+
+    page2.drawImage(imagemEmb2, {
+        x: 0,
+        y: 0,
+        width: 595,
+        height: 842,
+    });
+
+    // Salvar e baixar o PDF
+    const pdfBytes = await pdfDoc.save();
+    download(pdfBytes, "Eduarda_Gomes.pdf", "application/pdf");
+}
--- src/RedesSociais/RedesSociais.jsx
+++ src/RedesSociais/RedesSociais.jsx
@@ -1,4 +1,3 @@
-
 import { RiWhatsappFill } from "react-icons/ri";
 import { BsInstagram } from "react-icons/bs";
 import { FaTelegram } from "react-icons/fa";
@@ -6,57 +5,50 @@ import { FaPinterest } from "react-icons/fa";
 import { FaTiktok } from "react-icons/fa";
 import { IoMdCloudDownload } from "react-icons/io";
 
-import './RedesSociais.css'
+import './RedesSociais.css';
+import { GerarPDF } from '../PdfComposit/PdfComposit.jsx'; // Importação correta
 
 export default function RedesSociais() {
 
     const irParaPinteres = () => {
-        window.open(
-          "https://pt.pinterest.com/dudaasgomes/", "_blank"
-        );
-      };
+        window.open("https://pt.pinterest.com/dudaasgomes/", "_blank");
+    };
 
-      function IrParaOInstagram(){
+    function IrParaOInstagram() {
         window.open('https://www.instagram.com/duda.sgomes/', '_blank');
     }
 
-    function IrParaOTelegram(){
+    function IrParaOTelegram() {
         window.open('https://t.me/dudasgomes', '_blank');
     }
 
-    function IrParaOTiktok(){
+    function IrParaOTiktok() {
         window.open('https://www.tiktok.com/@duda.sgomes?_t=ZM-8taVHCFZmLO&_r=1', '_blank');
     }
 
-    return(
+    return (
         <div>
             <div className='d-flex text-center mt-4 container justify-content-center transparent'>   
-                <button onClick={()=>IrParaOInstagram()} className='buttonRedesSociais'> 
-                        <BsInstagram className="  "/>
+                <button onClick={IrParaOInstagram} className='buttonRedesSociais'> 
+                    <BsInstagram />
                 </button>   
-                <button onClick={()=>IrParaOTelegram()} className='buttonRedesSociais'> 
-                        <FaTelegram  className=" "/>
+                <button onClick={IrParaOTelegram} className='buttonRedesSociais'> 
+                    <FaTelegram />
                 </button>   
-                <button onClick={()=>irParaPinteres()} className='buttonRedesSociais'> 
-                        <FaPinterest className=" "/>
+                <button onClick={irParaPinteres} className='buttonRedesSociais'> 
+                    <FaPinterest />
                 </button>     
-                <button onClick={()=>IrParaOTiktok()} className='buttonRedesSociais'> 
-                        <FaTiktok className=" "/>
+                <button onClick={IrParaOTiktok} className='buttonRedesSociais'> 
+                    <FaTiktok />
                 </button>                  
             </div>
 
-
             <div>
-                <button className="button-download">
+                <button className="button-download" onClick={GerarPDF}>
                     <IoMdCloudDownload className="icone-download"/>
-                    <p className="mt-3">
-                        Download my Composit as PDF
-                    </p>
+                    <p className="mt-3">Download my Composit as PDF</p>
                 </button>
             </div>
-
-
         </div>
-
-    )
+    );
 }
